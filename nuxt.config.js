b03ee5272938d47a3c9fbc30dc77fae51bd082bd
shu-pf <shugoena@gmail.com>
2021-04-26 22:49:46 +0900

    ogpの設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,7 @@ export default {
     titleTemplate: '活魚なみき | %s',
     htmlAttrs: {
       lang: 'ja',
+      prefix: 'og: http://ogp.me/ns#',
     },
     meta: [
       { charset: 'utf-8' },
@@ -16,6 +17,25 @@ export default {
         name: 'description',
         content:
           '阪神電鉄 西元町駅 より徒歩1分。元町商店街の6丁目。卸売りはもちろん、店内では、毎日その日に浜から仕入れた新鮮な魚を、食べやすいように調理をした上販売しております。店主が自ら競りが行われる浜へ行き、長年の経験、本物の目利きで最高の魚を選んで参ります。',
+      },
+      { hid: 'og:site_name', property: 'og:site_name', content: '活魚なみき' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://namiki.shop/' },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: '活魚 なみき | 海産物が安くて新鮮！ 神戸市中央区の鮮魚店',
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          '阪神電鉄 西元町駅 より徒歩1分。元町商店街の6丁目。卸売りはもちろん、店内では、毎日その日に浜から仕入れた新鮮な魚を、食べやすいように調理をした上販売しております。店主が自ら競りが行われる浜へ行き、長年の経験、本物の目利きで最高の魚を選んで参ります。',
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://namiki.shop/img/common/shop.png',
       },
     ],
     link: [
