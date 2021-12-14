d815546d7a634369564e66d017e6eeebdbfe29ba
あでり <61904065+shu-pf@users.noreply.github.com>
2021-12-14 22:53:10 +0900

    修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,8 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
-        href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css',
+        href:
+          'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css',
       },
     ],
   },
