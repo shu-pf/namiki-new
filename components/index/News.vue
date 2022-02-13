90b4e118351d551fe1975cc0898280a8125b408a
Shugo Furuse <shugoena@gmail.com>
2022-02-13 17:21:59 +0900

    feat: newsComponentの作成

---
+++ components/index/News.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="p-news">
+    <h2 class="p-news__title">お知らせ</h2>
+    <ul class="p-news__list">
+      <li v-for="post in posts" :key="post.fields.title">
+        <NuxtLink :to="{ path: `posts/${post.sys.id}` }">{{
+          post.fields.title
+        }}</NuxtLink>
+      </li>
+    </ul>
+  </section>
+</template>
+<script>
+export default {
+  props: {
+    // eslint-disable-next-line vue/require-default-prop
+    posts: Array,
+  },
+}
+</script>
